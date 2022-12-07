730bd2d016a045aa01dcb39428ed12324f57a749
Carminajuarez <wolfothenight@gmail.com>
2022-12-06 23:09:27 -0300

    Se le agrega 1eros componentes

--- ProyectoCarminaJuarez/src/app/app.module.ts
+++ ProyectoCarminaJuarez/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
